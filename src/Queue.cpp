b9a88bdce7c7f1e4a00c70c53bc4224cfcfb18dd
Maxim Ermolaev <makcoh4nk@mail.ru>
2016-11-20 16:12:02 +0300

    version 1.0

--- src/Queue.cpp
+++ src/Queue.cpp
@@ -1,2 +1,130 @@
 #include "Queue.h"
 
+float* Avg_ar(int w_size, int* array, int ar_size)
+{
+	if ((w_size < 1) || (w_size > ar_size))
+		throw 1;
+
+	MyQueue q;
+
+	int sum = 0;
+	float *avg = NULL;
+	avg = new float[7 - w_size + 1];
+
+	q.push(array[0]);
+
+	for (int i = 0; i < w_size; ++i)
+	{
+		sum += array[i];
+	}
+	for (int i = 0; i < 7 - w_size + 1; ++i)
+	{
+		avg[i] = (double)sum / w_size;
+		sum = sum - q.front() + array[i + w_size];
+		q.pop();
+		q.push(array[i + 1]);
+	}
+
+	return avg;
+}
+
+
+MyQueue::MyQueue()
+{
+	begin = NULL;
+	end = NULL;
+}
+
+MyQueue::~MyQueue()
+{
+	while (begin != end)
+	{
+		CNode* temp = begin;
+		begin = temp->next;
+		temp->prev = NULL;
+		temp->next = NULL;
+		temp = NULL;
+		delete temp;
+	}
+	begin = NULL;
+	end = NULL;
+}
+
+void MyQueue::push(int data)
+{
+	CNode* temp = new CNode;
+	temp->data = data;
+	temp->prev = NULL;
+	if (begin != NULL)
+	{
+		temp->next = begin;
+		begin->prev = temp;
+		begin = temp;
+	}
+	else
+	{
+		temp->next = NULL;
+		begin = end = temp;
+	}
+}
+
+void MyQueue::pop()
+{
+	if (!empty())
+	{
+		if (end == begin)
+		{
+			end = NULL;
+			begin = NULL;
+			return;
+		}
+		CNode* temp = end;
+		end = temp->prev;
+		end->prev = temp->prev->prev;
+		end->next = NULL;
+	}
+	else
+		throw 1;
+}
+
+int MyQueue::front()
+{
+	if (!empty())
+		return end->data;
+	else
+		throw 1;
+}
+
+int MyQueue::back()
+{
+	if (!empty())
+		return begin->data;
+	else
+		throw 1;
+}
+
+bool MyQueue::empty()
+{
+	if ((begin == NULL) && (end == NULL))
+		return true;
+	else
+		return false;
+
+}
+
+int MyQueue::size()
+{
+	int count = 0;
+	if (empty())
+		return 0;
+	else
+	{
+		CNode* tmp = begin;
+		while (tmp != end)
+		{
+			count++;
+			tmp = tmp->next;
+		}
+		return (count + 1);
+	}
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,131 @@
 #include <gtest/gtest.h>
 #include "Queue.h"
 
+TEST(myQueue, can_create_empty_queue)
+{
+	MyQueue q;
+	EXPECT_TRUE(q.empty());
+}
+
+TEST(myQueue, can_push_and_get_front_elem)
+{
+	MyQueue q;
+	q.push(5);
+	q.push(10);
+	EXPECT_EQ(5, q.front());
+}
+
+TEST(myQueue, cannot_use_front_for_empty_queue)
+{
+	MyQueue q;
+	EXPECT_ANY_THROW(q.front());
+}
+
+TEST(myQueue, can_push_and_get_back_elem)
+{
+	MyQueue q;
+	q.push(5);
+	q.push(10);
+	EXPECT_EQ(10, q.back());
+}
+
+TEST(myQueue, cannot_use_back_for_empty_queue)
+{
+	MyQueue q;
+	EXPECT_ANY_THROW(q.back());
+}
+
+TEST(myQueue, can_pop_and_empty_queue_test)
+{
+	MyQueue q;
+	q.push(5);
+	q.push(10);
+	q.pop();
+	q.pop();
+	EXPECT_TRUE(q.empty());
+}
+
+TEST(myQueue, not_empty_queue_test)
+{
+	MyQueue q;
+	q.push(5);
+	q.push(10);
+	q.pop();
+	EXPECT_FALSE(q.empty());
+}
+
+TEST(myQueue, cannot_use_pop_for_empty_queue)
+{
+	MyQueue q;
+	EXPECT_ANY_THROW(q.pop());
+}
+
+TEST(myQueue, correct_size)
+{
+	MyQueue q;
+	q.push(5);
+	q.push(10);
+	EXPECT_EQ(2, q.size());
+}
+
+TEST(myQueue, correct_average_array_test_1)
+{
+	MyQueue q;
+	const int w = 3;
+	const int size = 5;
+	int ar[size] = { 5,9,7,2,3 };
+	float ex_ar[] = { 7,6,4 };
+
+	float* res_ar = Avg_ar(w, ar, size);
+
+	for (int i = 0; i < 3; ++i)
+		EXPECT_EQ(res_ar[i], ex_ar[i]);
+}
+
+TEST(myQueue, correct_average_array_test_2)
+{
+	MyQueue q;
+	const int w = 4;
+	const int size = 7;
+	int ar[size] = { 1,6,4,9,7,8,1 };
+	float ex_ar[] = { 5, 6.5, 7, 6.25 };
+
+	float* res_ar = Avg_ar(w, ar, size);
+
+	for (int i = 0; i < 3; ++i)
+		EXPECT_EQ(res_ar[i], ex_ar[i]);
+}
+
+TEST(myQueue, cannot_use_negative_window_size)
+{
+	MyQueue q;
+	const int w = -1;
+	const int size = 3;
+	int ar[size] = { 1,2,3 };
+
+	EXPECT_ANY_THROW(float* res_ar = Avg_ar(w, ar, size));
+}
+
+TEST(myQueue, cannot_use_zero_window_size)
+{
+	MyQueue q;
+	const int w = 0;
+	const int size = 3;
+	int ar[size] = { 1,2,3 };
+
+	EXPECT_ANY_THROW(float* res_ar = Avg_ar(w, ar, size));
+}
+
+TEST(myQueue, cannot_use_if_window_size_more_than_array_size)
+{
+	MyQueue q;
+	const int w = 4;
+	const int size = 3;
+	int ar[size] = { 1,2,3 };
+
+	EXPECT_ANY_THROW(float* res_ar = Avg_ar(w, ar, size));
+}
+
 int main(int ac, char* av[])
 {
   testing::InitGoogleTest(&ac, av);
